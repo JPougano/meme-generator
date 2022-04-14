0fc9f6b18fc6880d3876cda61e36bb0899593b2b
JPougano <pedroougano@gmail.com>
2022-04-14 00:46:38 -0300

    Finalização da parte de API

--- src/Inputs.js
+++ src/Inputs.js
@@ -1,48 +1,51 @@
 import React from "react";
-import Meme from "./memeData"
 
 export default function Inputs(){
 
-    const [memeImage, setMemeImage] = React.useState("https://i.imgflip.com/1b42wl.jpg");
-    // Gera um meme aleatório dentro da nossa coleção de memes
-        function changeMeme(){
-            const memesArray = Meme.data.memes
-            const randomNumber = Math.floor(Math.random() * memesArray.length)  
-            const newImage = memesArray[randomNumber].url
-            setMemeImage(newImage)
-        }
-    // Cria os textos que irão acima e abaixo do meme
-    const [memeText, setMemeText] = React.useState({
+    const [meme, setMeme] = React.useState({
         topText: "",
-        bottomText: ""
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
-
-    // Altera o state que contém o texto de cima do meme
-    function changeText(event){
-
-        const {name,value} = event.target;
-        setMemeText(prevText => {
-            return {
-                ...prevText,
-                [name]:value
-            }
-        })
+    const [allMemes, setAllMemes] = React.useState([])
+    
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+    
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
     }
-console.log(memeText)
+    
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+    
     // Retorna o meu componente completo
     return (
         <section className="flex inputs--container">
             <div className="flex inputs--input---subcontainer">
-                    <input onKeyUp={changeText} name="topText" placeholder="Top text" id="input1"type="text"/>
-                    <input onKeyUp={changeText} name="bottomText" placeholder="Bottom text" id="input2" type="text"/>
+                    <input onChange={handleChange} name="topText" placeholder="Top text" id="input1"type="text"/>
+                    <input onChange={handleChange} name="bottomText" placeholder="Bottom text" id="input2" type="text"/>
             </div>
-            <button onClick={changeMeme} >Get a new meme image  🖼</button>
+            <button onClick={getMemeImage} >Get a new meme image  🖼</button>
             <div className="flex input--meme---subcontainer">
-                <p className="top-input input--image---text">{memeText.topText}</p>
-                <img className="inputs--meme" src={memeImage} alt="meme image"/>
-                <p className="bottom-input input--image---text">{memeText.bottomText}</p>
+                <p className="top-input input--image---text">{meme.topText}</p>
+                <img className="inputs--meme" src={meme.randomImage} alt="meme image"/>
+                <p className="bottom-input input--image---text">{meme.bottomText}</p>
             </div>
         </section>
     )
 }
-
